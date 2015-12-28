16253e5a347f12d6cb13717765bfc053a9fc288f
mutantu2002 <mutantu2002@yahoo.com>
2015-12-28 13:48:46 +0200

    Add frames in kernel

--- src/main/java/home/mutant/dl/utils/kmeans/smooth/RunSmoothieOpenCl.java
+++ src/main/java/home/mutant/dl/utils/kmeans/smooth/RunSmoothieOpenCl.java
@@ -3,7 +3,7 @@ package home.mutant.dl.utils.kmeans.smooth;
 import home.mutant.dl.utils.kmeans.model.ListClusterable;
 
 public class RunSmoothieOpenCl {
-	public static final int FRAMES = 1;
+	public static final int FRAMES = 100;
 	public static void main(String[] args) {
 		
 		ListClusterable filters = ListClusterable.load("clusters4_256");
@@ -13,9 +13,9 @@ public class RunSmoothieOpenCl {
 		for(int i=0;i<FRAMES;i++){
 			sm.stepV();
 			//sm.stepX();
-			if(i%1000==0)
-				sm.show();
-			if(i%1000==0)System.out.println(i);
+			//if(i%1==0)
+			//	sm.show();
+			//System.out.println(i);
 		}
 		System.out.println("FPS:" + (1000.*FRAMES/(System.currentTimeMillis()-t0)));
 		//sm.listDistances();
--- src/main/resources/opencl/ElasticSmoothie.c
+++ src/main/resources/opencl/ElasticSmoothie.c
@@ -9,27 +9,46 @@ __kernel void stepV(__global double *x, __global double *y,__global double *vx, 
 	int j;
 	double dx;
 	double dy;
+	double lvx;
+	double lvy;
 	int f;
+	double d;
+
 	lx[i]=x[i];
 	ly[i]=y[i];
+	lvx=vx[i];
+	lvy=vy[i];
 
 	barrier(CLK_LOCAL_MEM_FENCE);
 
 	for (f=0;f<1000;f++)
 	{
-	for (j=0;j<NO_PARTICLES;j++){
-		if (i==j)continue;
-		dx=lx[i]-lx[j];
-		dy=ly[i]-ly[j];
-		double d = sqrt(dx*dx+dy*dy);
-		fx+=dx/d*(preDistances[i*NO_PARTICLES+j]-d)*K;
-		fy+=dy/d*(preDistances[i*NO_PARTICLES+j]-d)*K;
+		fx=0;
+		fy=0;
+		for (j=0;j<NO_PARTICLES;j++){
+			if (i==j)continue;
+			dx=lx[i]-lx[j];
+			dy=ly[i]-ly[j];
+			d = sqrt(dx*dx+dy*dy);
+			fx+=dx/d*(preDistances[i*NO_PARTICLES+j]-d)*K;
+			fy+=dy/d*(preDistances[i*NO_PARTICLES+j]-d)*K;
+		}
+		fx-=lvx*FRICTION;
+		fy-=lvy*FRICTION;
+		lvx+=fx*DT;
+		lvy+=fy*DT;
+
+		barrier(CLK_LOCAL_MEM_FENCE);
+
+		lx[i]+=lvx*DT;
+		ly[i]+=lvy*DT;
+
+		barrier(CLK_LOCAL_MEM_FENCE);
 	}
-	fx-=vx[i]*FRICTION;
-	fy-=vy[i]*FRICTION;
-	vx[i]+=fx*DT;
-	vy[i]+=fy*DT;
-	}
+	x[i]=lx[i];
+	y[i]=ly[i];
+	vx[i]=lvx;
+	vy[i]=lvy;
 }
 
 __kernel void stepX(__global double *x, __global double *y,__global double *vx, __global double *vy)
